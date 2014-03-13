3db2823ef3aa6ccc71e167afcb18bba18bcf69bb
zeevro@gmail.com <zeevro@gmail.com@8b18f9a5-21ca-81fb-ec04-82c933a75c7c>
2014-03-13 23:15:39 +0000

    Back port from RFNet32

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <GenericTypeDefs.h>
+#include <xc.h>
+#include <plib.h>
+#include <string.h>
+
+#include "queue.h"
+#include "protocol.h"
+#include "hal.h"
+#include "nRF24L01P.h"
+
+#define BIT_0 (1 << 0)
+#define BIT_1 (1 << 1)
+#define BIT_2 (1 << 2)
+#define BIT_3 (1 << 3)
+#define BIT_4 (1 << 4)
+#define BIT_5 (1 << 5)
+#define BIT_6 (1 << 6)
+#define BIT_7 (1 << 7)
+
+#endif /* _MAIN_H */
